565e2e2fb3d3a2f5c471b5f77e8b463bbe2c3e14
duskmoon (Campbell He) <kp.campbell.he@duskmoon314.com>
2025-05-14 19:58:02 +0800

    feat: add flag for modify authorized_keys file

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::path::PathBuf;
 use clap::Parser;
 use kanidm_client::{ClientError, KanidmClient, KanidmClientBuilder};
 use serde::{Deserialize, Serialize};
-use tracing::error;
+use tracing::{debug, error};
+
+const SSH_CONFIG_DIR: &str = "~/.ssh";
 
 #[derive(Debug, Parser, Serialize, Deserialize)]
 #[command(version, about)]
@@ -27,6 +29,13 @@ pub struct Cli {
     /// The account ids to fetch, space separated
     #[serde(default)]
     account_ids: Vec<String>,
+
+    /// Whether to modify the authorized_keys file
+    ///
+    /// If true, the program will try to update ~/.ssh/authorized_keys
+    #[arg(short, long, default_value_t = false)]
+    #[serde(default)]
+    modify: bool,
 }
 
 impl Cli {
@@ -35,6 +44,7 @@ impl Cli {
         self.addr = self.addr.clone().or(other.addr.clone());
         self.ca_path = self.ca_path.clone().or(other.ca_path.clone());
         self.account_ids.extend(other.account_ids.clone());
+        self.modify = self.modify || other.modify;
     }
 }
 
@@ -79,16 +89,68 @@ pub fn build_configured_client(args: &Cli) -> Result<KanidmClient, ()> {
     })
 }
 
+pub fn modify_authorized_keys(keys: Vec<String>) -> Result<(), ()> {
+    debug!("Modifying authorized_keys file started");
+
+    let ssh_config_dir = PathBuf::from(shellexpand::tilde(SSH_CONFIG_DIR).into_owned());
+    if !ssh_config_dir.exists() {
+        debug!("Creating ssh config directory");
+
+        std::fs::create_dir(&ssh_config_dir)
+            .map_err(|e| error!("Failed to create ssh config directory -- {:?}", e))?;
+    }
+
+    let authorized_keys_file = ssh_config_dir.join("authorized_keys");
+
+    let mut authorized_keys =
+        std::fs::read_to_string(&authorized_keys_file).unwrap_or_else(|_| String::new());
+
+    // Find `# Managed Keys by kanidm_sshkey_fetcher` and `# End of Managed Keys by kanidm_sshkey_fetcher`
+    const MANAGED_KEYS_START: &str = "# Managed Keys by kanidm_sshkey_fetcher";
+    const MANAGED_KEYS_END: &str = "# End of Managed Keys by kanidm_sshkey_fetcher";
+    let start_index = authorized_keys
+        .find(MANAGED_KEYS_START)
+        .unwrap_or(authorized_keys.len());
+    let end_index = authorized_keys
+        .find(MANAGED_KEYS_END)
+        .unwrap_or(authorized_keys.len());
+
+    // Prepare the new content
+    let mut new_content = String::new();
+    for key in keys {
+        new_content.push_str(&format!("{}\n", key));
+    }
+
+    // Replace the managed keys section if it exists
+    if start_index < end_index {
+        let start_index = start_index + MANAGED_KEYS_START.len() + 2; // +2 for the newline
+        new_content.push('\n'); // Add a newline between the content and the end marker
+        authorized_keys.replace_range(start_index..end_index, &new_content);
+    } else {
+        // If the section doesn't exist, append the new content
+        authorized_keys.push_str(&format!(
+            "\n{}\n\n{}\n{}\n",
+            MANAGED_KEYS_START, new_content, MANAGED_KEYS_END
+        ));
+    }
+
+    // Write the updated content back to the file
+    std::fs::write(&authorized_keys_file, authorized_keys)
+        .map_err(|e| error!("Failed to write to authorized_keys file -- {:?}", e))?;
+
+    Ok(())
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), ()> {
     let mut args = Cli::parse();
 
     if let Some(config_path) = &args.config_path {
         let config_content = std::fs::read_to_string(config_path)
-            .map_err(|e| error!("Failed to read config file -- {:?}", e))?;
+            .map_err(|e| eprintln!("Failed to read config file -- {:?}", e))?;
 
         let args_file: Cli = toml::from_str(&config_content)
-            .map_err(|e| error!("Failed to parse config file -- {:?}", e))?;
+            .map_err(|e| eprintln!("Failed to parse config file -- {:?}", e))?;
 
         args.or(&args_file);
     }
@@ -112,12 +174,22 @@ async fn main() -> Result<(), ()> {
         }
     }
 
+    let mut keys = Vec::new();
+
     for id in &args.account_ids {
         match client.idm_account_get_ssh_pubkeys(id.as_str()).await {
-            Ok(pkeys) => pkeys.iter().for_each(|pkey| println!("{}", pkey)),
+            Ok(pkeys) => {
+                keys.extend(pkeys.clone());
+                pkeys.iter().for_each(|pkey| println!("{}", pkey))
+            }
             // Err(e) => error!("Failed to get ssh pubkeys for account {} -- {:?}", id, e),
             Err(_e) => {}
         }
+    }
+
+    // Modify the authorized_keys file if requested
+    if args.modify {
+        modify_authorized_keys(keys)?;
     }
 
     Ok(())
